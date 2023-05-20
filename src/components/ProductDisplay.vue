77162fe50dbefc7d4aeee954cd3cbeab428a7043
RaihanEdwin <raihanewin@gmail.com>
2023-05-20 14:27:19 +0700

    first commit

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    data(){
+        return{
+            number: 1
+        }
+    },
+    methods:{
+
+    },
+}
+
+</script>
+
+<template>
+    <div class="flex">
+        <div class="w-1/2 h-screen bg-gray-200">
+            tes
+        </div>
+        <div class="w-1/2 flex flex-col">
+            <div>
+                <h1>Greyna Sofa</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, cupiditate porro. Ab repellat tenetur expedita ipsam, nisi nulla libero impedit repudiandae illo deserunt quisquam et accusantium suscipit veniam iusto doloribus!</p>
+            </div>
+        </div>
+    </div>
+</template>
